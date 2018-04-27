8a76d2b2c09527d70a4df91f02cfeb160d07fbbb
Veronica <vmvalenzuela24@gmail.com>
2018-04-26 22:04:20 -0700

    Colors now change according to the selection of the dropdown menu.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var rectangleColor = '#0099ff';
+
+// Detects mouse movement on the canvas to draw the rectangles.
 function initDraw(canvas) {
     function setMousePosition(e) {
         var ev = e || window.event; //Moz || IE
@@ -39,8 +42,38 @@ function initDraw(canvas) {
             element.className = 'rectangle';
             element.style.left = mouse.x + 'px';
             element.style.top = mouse.y + 'px';
+			element.style.borderColor = rectangleColor;
             canvas.appendChild(element);
             canvas.style.cursor = "crosshair";
         }
     };
+}
+
+
+// Clear labels function to clear all labels from the canvas.
+// Deletes each child of the canvas object individually using removeChild()
+// Can be modified to remove just the most recent child, oldest child, etc.
+function clearLabels() {
+	var canv = document.getElementById('canvas');
+	while (canv.firstChild) {
+    	canv.removeChild(canv.firstChild);
+	}
+}
+
+
+// Change the color of the rectangle depending on the item selected in the drop down.
+function changeColor() {
+	var value = document.getElementById('dropdown').value;
+	if(value === 'one') {
+		rectangleColor = '#0099ff'; //blue
+	} else if(value === 'two') {
+		rectangleColor = '#29a329'; //green
+	} else if(value === 'three') { 
+		rectangleColor = '#ffb3d9'; //pink
+	} else if(value === 'four') {
+		rectangleColor = '#ffa64d'; //orange
+	} else if(value === 'five') {
+		rectangleColor = '#ffff66'; //yellow
+	}
+	
 }
